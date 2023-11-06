849a319997a305699f50cb713577c837beb44215
Klushin Valdimir <vova_klushin@mail.ru>
2023-11-07 01:18:29 +0300

    1st

---
+++ app/build.gradle.kts
@@ -0,0 +1,83 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("androidx.navigation.safeargs")
+}
+
+android {
+    namespace = "com.test.weather"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.test.weather"
+        minSdk = 24
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        buildConfigField ("String", "BASE_URL", "\"https://api.weatherapi.com/v1/\"")
+        buildConfigField ("String", "API_KEY", "\"fd2bdb4f9469438bbd7165838230411\"")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.10.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    val nav_version = "2.7.5"
+
+    // Kotlin
+    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
+    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
+
+
+// architecture components
+    val coreVersion = "1.7.0"
+    val lifecycleVersion = "2.4.0"
+    val pagingVersion = "3.1.0"
+    implementation ("androidx.core:core-ktx:$coreVersion")
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-savedstate:$lifecycleVersion")
+    implementation ("androidx.paging:paging-runtime-ktx:$pagingVersion")
+
+    val retrofitVersion = "2.9.0"
+    val okhttpLoggingInterceptorVersion = "4.9.0"
+    val moshiVersion = "1.12.0"
+    // retrofit
+    implementation ("com.squareup.retrofit2:retrofit:$retrofitVersion")
+    implementation ("com.squareup.retrofit2:converter-moshi:$retrofitVersion")
+    implementation ("com.squareup.moshi:moshi:$moshiVersion")
+    implementation ("com.squareup.moshi:moshi-kotlin:$moshiVersion")
+    implementation ("com.squareup.retrofit2:retrofit-mock:$retrofitVersion")
+    implementation ("com.squareup.okhttp3:logging-interceptor:$okhttpLoggingInterceptorVersion")
+    implementation ("com.squareup.retrofit2:converter-gson:$retrofitVersion")
+
+    implementation ("com.google.android.gms:play-services-location:21.0.1")
+}
---
+++ app/src/main/java/com/test/weather/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.test.weather
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity(R.layout.activity_main) {}
